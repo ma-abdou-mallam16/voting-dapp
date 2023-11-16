dc33bd5f1c1bbb2d6db3a84aa5740fbca1fb2852
Abdou Mallam Mahamadou Aminou <ma.abdou.mallam@gmail.com>
2023-11-16 01:25:52 +0100

    first commit

---
+++ hardhat/hardhat.config.js
@@ -0,0 +1,26 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {
+      blockGasLimit: 3000000 // ! Default 30_000_000
+    },
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+  },
+};
